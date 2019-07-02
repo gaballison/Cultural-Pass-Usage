1f5c5eaf4b678f8abe41bb4553461f6593f76c66
Allison Fredrickson <allisonfred@gmail.com>
2019-07-02 14:23:03 -0400

    Initial commit of Visual Studio project

---
+++ CulturalPassUsage/Cultural Pass Usage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cultural_Pass_Usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
