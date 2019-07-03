0bc716b540c1b689daa0a46732124a8296c3a472
Allison Fredrickson <gaballison@users.noreply.github.com>
2019-07-02 23:02:26 -0400

    Add skeleton classes

---
+++ CulturalPassUsage/Cultural Pass Usage/Pass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cultural_Pass_Usage
+{
+    class Pass
+    {
+        //TO DO: RegEx to verify input is a 6-digit number
+
+
+    }
+}
---
+++ CulturalPassUsage/Cultural Pass Usage/Venue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cultural_Pass_Usage
+{
+    class Venue
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        // needs to have list of all pass numbers
+    }
+}
